4514b973a0c63226cd9748abde6a653171409506
Jorge <jorge.mpavon@alumnos.upm.es>
2023-09-29 21:13:55 +0200

    develop fixed coverage

--- src/test/java/es/upm/miw/iwg_devops/code/FractionTest.java
+++ src/test/java/es/upm/miw/iwg_devops/code/FractionTest.java
@@ -1,54 +1,56 @@
 package es.upm.miw.iwg_devops.code;
 
 import static org.junit.Assert.*;
-
+import org.junit.Before;
 import es.upm.miw.iwvg_devops.code.Fraction;
 
 import org.junit.Test;
 
 public class FractionTest {
+    private Fraction fraction;
 
-
-
-    @Test
-    public void testConstructorWithArguments() {
-        Fraction fraction = new Fraction(3, 4);
-        assertEquals(3, fraction.getNumerator());
-        assertEquals(4, fraction.getDenominator());
+    @Before
+    public void setUp() {
+        fraction = new Fraction(3, 4);
     }
 
     @Test
-    public  void testDefaultConstructor() {
-        Fraction fraction = new Fraction();
-        assertEquals(1, fraction.getNumerator());
-        assertEquals(1, fraction.getDenominator());
+    public void testGetNumerator() {
+        assertEquals(3, fraction.getNumerator());
     }
 
     @Test
     public void testSetNumerator() {
-        Fraction fraction = new Fraction();
         fraction.setNumerator(5);
         assertEquals(5, fraction.getNumerator());
     }
 
     @Test
+    public void testGetDenominator() {
+        assertEquals(4, fraction.getDenominator());
+    }
+
+    @Test
     public void testSetDenominator() {
-        Fraction fraction = new Fraction();
-        fraction.setDenominator(6);
-        assertEquals(6, fraction.getDenominator());
+        fraction.setDenominator(7);
+        assertEquals(7, fraction.getDenominator());
     }
 
     @Test
     public void testDecimal() {
-        Fraction fraction = new Fraction(1, 2);
-        assertEquals(0.5, fraction.decimal(), 0.001);
+        assertEquals(0.75, fraction.decimal(), 0.001); // Using delta for floating-point comparisons
+    }
+
+    @Test
+    public void testDefaultConstructor() {
+        Fraction defaultFraction = new Fraction();
+        assertEquals(1, defaultFraction.getNumerator());
+        assertEquals(1, defaultFraction.getDenominator());
     }
 
     @Test
     public void testToString() {
-        Fraction fraction = new Fraction(2, 3);
-        assertEquals("Fraction{numerator=2, denominator=3}", fraction.toString());
+        assertEquals("Fraction{numerator=3, denominator=4}", fraction.toString());
     }
-
 
 }
--- src/test/java/es/upm/miw/iwg_devops/code/UserTest.java
+++ src/test/java/es/upm/miw/iwg_devops/code/UserTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 
 import es.upm.miw.iwvg_devops.code.Fraction;
 import es.upm.miw.iwvg_devops.code.User;
-
+import org.junit.Before;
 import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,77 +13,73 @@ import java.util.Arrays;
 
 public class UserTest {
 
+    private User user;
+
+    @Before
+    public void setUp() {
+        user = new User("123", "Jorge", "Muñoz", new ArrayList<>());
+    }
+
     @Test
     public void testGetId() {
-        User user = new User("1", "Jorge", "Muñoz", new ArrayList<>());
-        assertEquals("1", user.getId());
+        assertEquals("123", user.getId());
     }
 
     @Test
     public void testGetName() {
-        User user = new User("1", "Jorge", "Muñoz", new ArrayList<>());
         assertEquals("Jorge", user.getName());
     }
 
     @Test
     public void testSetName() {
-        User user = new User("1", "Jorge", "Muñoz", new ArrayList<>());
-        user.setName("Alice");
-        assertEquals("Alice", user.getName());
+        user.setName("Jorge");
+        assertEquals("Jorge", user.getName());
     }
 
     @Test
     public void testGetFamilyName() {
-        User user = new User("1", "Jorge", "Muñoz", new ArrayList<>());
         assertEquals("Muñoz", user.getFamilyName());
     }
 
     @Test
     public void testSetFamilyName() {
-        User user = new User("1", "Jorge", "Muñoz", new ArrayList<>());
         user.setFamilyName("Muñoz");
         assertEquals("Muñoz", user.getFamilyName());
     }
 
     @Test
     public void testGetFractions() {
-        List<Fraction> fractions = Arrays.asList(new Fraction(1, 2), new Fraction(3, 4));
-        User user = new User("1", "Jorge", "Muñoz", fractions);
-        assertEquals(fractions, user.getFractions());
+        assertNotNull(user.getFractions());
+    }
+
+    @Test
+    public void testSetFractions() {
+        List<Fraction> fractions = new ArrayList<>();
+        fractions.add(new Fraction(1, 2));
+        user.setFractions(fractions);
+
+        assertEquals(1, user.getFractions().size());
     }
 
     @Test
     public void testAddFraction() {
-        User user = new User("1", "Jorge", "Muñoz", new ArrayList<>());
-        Fraction fraction = new Fraction(1, 2);
-        user.addFraction(fraction);
+        user.addFraction(new Fraction(1, 3));
         assertEquals(1, user.getFractions().size());
-        assertEquals(fraction, user.getFractions().get(0));
+    }
+
+    @Test
+    public void testFullName() {
+        assertEquals("Jorge Muñoz", user.fullName());
     }
 
     @Test
     public void testInitials() {
-        User user = new User("1", "Jorge", "Muñoz", new ArrayList<>());
         assertEquals("J.", user.initials());
     }
 
     @Test
     public void testToString() {
-        User user = new User("1", "Jorge", "Muñoz", Arrays.asList(new Fraction(1, 2), new Fraction(3, 4)));
-        assertEquals("User{id='1', name='Jorge', familyName='Muñoz', fractions=[Fraction{numerator=1, denominator=2}, Fraction{numerator=3, denominator=4}]}", user.toString());
-    }
-    @Test
-    public void testFullName() {
-        User user = new User("1", "Jorge", "Muñoz", null);
-        assertEquals("Jorge Muñoz", user.fullName());
-    }
-
-    @Test
-    public void testSetFractions() {
-        User user = new User("1", "Jorge", "Muñoz", new ArrayList<>());
-        List<Fraction> fractions = Arrays.asList(new Fraction(1, 2), new Fraction(3, 4));
-        user.setFractions(fractions);
-        assertEquals(fractions, user.getFractions());
+        assertEquals("User{id='123', name='Jorge', familyName='Muñoz', fractions=[]}", user.toString());
     }
 }
 
